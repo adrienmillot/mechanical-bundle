5ef82895bdebd08f27dce00fc931e97f5b1ab4e0
Adrien <adrien.millot@gmail.com>
2020-07-21 14:57:28 +0200

    Bundle initialize

---
+++ MechanicalBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace amillot\MechanicalBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class MechanicalBundle
+ *
+ * @author Adrien MILLOT <adrien.millot@gmail.com>
+ * @license For the full copyright and license information, please view the LICENSE
+ *          file that was distributed with this source code.
+ */
+class MechanicalBundle extends Bundle
+{
+}
