2265d826d7b16bd8bbd6dce7a5dd631cbf593506
Adrien <adrien.millot@gmail.com>
2020-07-25 17:23:02 +0200

    Add Engine entity

---
+++ Model/EngineInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace amillot\MechanicalBundle\Model;
+
+use amillot\MechanicalBundle\Model\Traits\NamableInterface;
+
+/**
+ * Interface EngineInterface
+ *
+ * @author Adrien MILLOT <adrien.millot@gmail.com>
+ */
+interface EngineInterface extends NamableInterface
+{
+}
